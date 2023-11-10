80f357a30895a9553bfcf7e24d027886cd2c11fe
Akos-1 <veronicagyasi@gmail.com>
2023-11-09 21:03:07 -0800

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+# Bash script for setting up web servers for the deployment of web_static
+
+# Update package information
 sudo apt-get update
+
+# Install Nginx
 sudo apt-get -y install nginx
+
+# Allow Nginx through the firewall
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+# Create directory structure
+sudo mkdir -p /data/web_static/{releases/test,shared}
+
+# Create index.html with a simple message
 sudo echo "<html>
   <head>
   </head>
@@ -18,10 +22,14 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
+# Create a symbolic link to the 'current' release
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
+# Set ownership to the user 'ubuntu'
 sudo chown -R ubuntu:ubuntu /data/
 
+# Update Nginx configuration to include the static files
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
+# Restart Nginx for changes to take effect
 sudo service nginx restart
