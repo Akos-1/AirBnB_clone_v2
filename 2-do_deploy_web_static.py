4bf56d2f203c487b6bef2b29b9741c2cc7bfd422
Akos-1 <veronicagyasi@gmail.com>
2023-11-12 12:58:18 -0800

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,22 +27,19 @@ def do_deploy(archive_path):
         run("mkdir -p /data/web_static/releases/{}".format(filename))
 
         # Uncompress the archive to /data/web_static/releases/<filename>
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            filename + '.tgz', filename))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename + '.tgz', filename))
 
         # Remove the archive from the web server
         run("rm /tmp/{}".format(filename + '.tgz'))
 
         # Move contents to the current version
-        run("mv /data/web_static/releases/{}/web_static/*"
-            "/data/web_static/releases/{}/".format(filename, filename))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
 
         # Remove the old symbolic link
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link linked to the new version
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
 
         print("New version deployed!")
 
