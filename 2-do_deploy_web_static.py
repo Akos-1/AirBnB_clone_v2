bf80e2edb1bc3c01984d85edca9baeab719d2df8
Akos-1 <veronicagyasi@gmail.com>
2023-11-12 12:06:38 -0800

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from fabric.api import env, put, run, local
+from os.path import exists
+
+# Set the user and hosts using Fabric environment variables
+env.user = 'ubuntu'
+env.hosts = ['52.91.157.33', '3.85.148.37']
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+
+    # Check if the file at archive_path exists
+    if not exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Get the filename without extension
+        filename = archive_path.split('/')[-1].split('.')[0]
+
+        # Create the releases folder if it doesn't exist
+        run("mkdir -p /data/web_static/releases/{}".format(filename))
+
+        # Uncompress the archive to /data/web_static/releases/<filename> on the web server
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename + '.tgz', filename))
+
+        # Remove the archive from the web server
+        run("rm /tmp/{}".format(filename + '.tgz'))
+
+        # Move contents to the current version
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+
+        # Remove the old symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link linked to the new version
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+
+        print("New version deployed!")
+
+        return True
+
+    except Exception as e:
+        print("Error: {}".format(e))
+        return False
