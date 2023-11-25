be65a64dc63d0e476bcfa6d3f37585a31b465c65
Akos-1 <veronicagyasi@gmail.com>
2023-11-25 12:00:53 -0800

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,20 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when the root URL is accessed.
+
+    Returns:
+           A given string
+    """
     return "Hello HBNB!"
 
-
 if __name__ == "__main__":
+    """
+    Runs the Flask application on 0.0.0.0:5000.
+
+    Debug mode is disabled.
+
+    To test the application, open a web browser and go to http://0.0.0.0:5000/
+    """
     app.run(host="0.0.0.0", port=5000, debug=None)
